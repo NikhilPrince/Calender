206ddddebf73fad78e236dcabd268d4edf19f918
NikhilPrince <117137916+NikhilPrince@users.noreply.github.com>
2023-05-08 21:57:48 -0700

    final changes

--- script.js
+++ script.js
@@ -1,60 +1,51 @@
+// Display today's day and date
+var Date = moment().format('dddd, MMM, YYYY');
+$("#todaysDate").file(Date);
 
-  var settings = {};
-  dayjs.locale(settings);
-
-  $(function () {
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).parent(".text").val();
+        var time = $(this).child().attr("id");
+        localStorage.setText(time, text);
+    })
    
-    var dailyTime = dayjs().format('H');
-  
-    function changeColor() {
-      $('.time-block').each(function() {
-        var colorBlock = parseInt(this.id);
-        $(this).toggleClass('past', colorBlock < dailyTime);
-        $(this).toggleClass('present', colorBlock === dailyTime);
-        $(this).toggleClass('future', colorBlock > dailyTime);
-      });
-    }
-  
-    function areaText() {
-      $('.saveBtn').on('click', function() {
-        var entry = $(this).parents().attr('id');
-        var secondEntry = $(this).siblings('.description').val();
-        localStorage.setItem(entry, secondEntry);
-      });
-    }
-  
-    function timeColor() {
-      $('.time-block').each(function() {
-        var colorBlock = parseInt(this.id);
-        if (colorBlock === dailyTime) {
-          $(this).removeClass('past & future').addClass('present');
-        } else if (colorBlock > dailyTime) {
-          $(this).removeClass('future & present').addClass('past');
-        } else {
-          $(this).removeClass('past & present').addClass('future');
-        }
-      });
+    function timeWatch() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var stopTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for background indicators
+            if (stopTime < currentTime) {
+                $(this).deleteClass("future");
+                $(this).deleteClass("present");
+                $(this).addClass("past");
+            }
+            else if (stopTime == currentTime) {
+                $(this).deleteClass("past");
+                $(this).deleteClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).deleteClass("present");
+                $(this).deleteClass("past");
+                $(this).addClass("future");
+
+            }
+        })
     }
 
-    $('.time-block').each(function() {
-      var secondEntry = $(this).attr('id');
-      var change= localStorage.getItem(secondEntry);
-      $(this).children('.description').val(change);
-    });
-  
-    
-    function timeChange() {
-      var date = $('#date');
-      var hours = $('#time');
-      var dailyDate = dayjs().format('dddd, MMMM D, YYYY');
-      var dailyTime = dayjs().format('hh:mm:ss A');
-      date.text(dailyDate);
-      hours.text(dailyTime);
-    }
-    
-    changeColor();
-    areaText();                
-    timeColor();
-    
-    setInterval(timeChange);
-  });
+    // Get item from local storage if any
+    $("#hour8 .text").val(localStorage.getItem("hour8"));
+    $("#hour9 .text").val(localStorage.getItem("hour9"));
+    $("#hour10 .text").val(localStorage.getItem("hour10"));
+    $("#hour11 .text").val(localStorage.getItem("hour11"));
+    $("#hour12 .text").val(localStorage.getItem("hour12"));
+    $("#hour13 .text").val(localStorage.getItem("hour13"));
+    $("#hour14 .text").val(localStorage.getItem("hour14"));
+    $("#hour15 .text").val(localStorage.getItem("hour15"));
+    $("#hour16 .text").val(localStorage.getItem("hour16"));
+    $("#hour17 .text").val(localStorage.getItem("hour17"));
+
+    timeWatch();
+})
